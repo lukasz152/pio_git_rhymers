5723171b8eea0c1c89924e2bb5cde2930b542be3
Łukasz <245968@edu.p.lodz.pl>
2024-05-29 12:31:28 +0200

    14. przeorganizowanie projektu tak aby usunac plik zawierajacy te klase i zmiana klasy publicznej tak gdzie nie musi byc

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -56,4 +56,35 @@ public class IntLinkedList {
     public void setI(int i) {
         this.i = i;
     }
+   private class Node {
+
+        private final int value;
+        private Node prev;
+        private Node next;
+
+        public Node(int i) {
+            value = i;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public Node getPrev() {
+            return prev;
+        }
+
+        public void setPrev(Node prev) {
+            this.prev = prev;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+    }
+
 }
